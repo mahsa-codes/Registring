1c88f75118c87544b7f2d9e6bc10b8e863e974ae
Mahsa <mahsa.nadery69@gmail.com>
2025-07-22 17:33:28 +0330

    login form is done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const user = localStorage.getItem("user");
+let user = localStorage.getItem("user");
 if(user) {
     let parsedUser =JSON.parse("user");
     document.getElementById('welcome').innerHTML = `Hello ${parsedUser}`;
@@ -10,4 +10,25 @@ if(user) {
 document.getElementById('logoutBtn').addEventListener('click', function(){
     localStorage.removeItem("user");
     window.location.href = "login.html"
-})
+});
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    let user = localStorage.getItem(username);
+    if(user) {
+        let parsedUser = JSON.parse(user);
+        if (parsedUser.password === password) {
+            localStorage.setItem("user",JSON.stringify(parsedUser));
+            window.location.href= "index.html";
+        } else {
+            alert ("Password is Incorrect");
+        }
+    } 
+    else {
+        alert("Username not found");
+    }
+});
